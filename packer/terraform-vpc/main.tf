440f551fe62112a5bfdb7d782637175883b549d2
ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-11-08 22:52:28 -0500

    packer: use terraform module to setup VPC for builds

---
+++ packer/terraform-vpc/main.tf
@@ -0,0 +1,25 @@
+module "vpc" {
+  source = "github.com/fpco/fpco-terraform-aws//packer/terraform-vpc?ref=master"
+  region = "${var.region}"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+// region
+output "region" {
+  value = "${var.region}"
+}
+// VPC ID
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
+// Subnet ID
+output "subnet_id" {
+  value = "${module.vpc.subnet_id}"
+}
+// ID of latest xenial AMI
+output "xenial_ami_id" {
+  value = "${module.vpc.xenial_ami_id}"
+}
