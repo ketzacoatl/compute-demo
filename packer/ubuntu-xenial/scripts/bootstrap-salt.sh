2221a4d04a7480e6860c52878838ec7a4fb298b5
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-11-30 03:01:54 -0500

    packer/xenial: drop unnecessary comments from bootstrap-salt script

--- packer/ubuntu-xenial/scripts/bootstrap-salt.sh
+++ packer/ubuntu-xenial/scripts/bootstrap-salt.sh
@@ -15,13 +15,6 @@ service salt-minion stop
 
 # the bootstrap formula might need git installed..
 apt-get install -y git
-# update SSH known hosts for strict host key verification and success with git.latest
-#salt-call --local                              \
-#          state.single ssh_known_hosts.present \
-#          --log-level=debug                    \
-#          name=github.com enc=ssh-rsa          \
-#          fingerprint_hash_type=sha256         \
-#          fingerprint=nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8
 
 # clone our salt formula to bootstrap salt formula
 salt-call --local                                             \
@@ -61,28 +54,3 @@ ls -alh /etc/salt/*
 cat /etc/salt/minion.d/*
 echo "version check:"
 salt-call --version
-
-# the following is a hack to a) allow us to continue running 2015.5 for it's
-# ext_pillar module, and b) get the pip state/module from 2015.8, which doesn't
-# break pip
-#wget --output-document=/usr/lib/python2.7/dist-packages/salt/utils/locales.py https://raw.githubusercontent.com/saltstack/salt/2015.8/salt/utils/locales.py
-# I really, really don't understand why this sleep works, but without it, the
-# sync_all in this production build fails to sync anything. If you are interested
-# in the details, see https://github.com/saltstack/salt/issues/34080
-#echo "pause to give time before we run sync_all"
-#sleep 60
-# only needed for the moment, while fpco-salt-formula puts consul ext_pillar
-# into this directory "manually", once that module is imported into the repo,
-# this snippet/hack could be removed. Here's the catch, that module cannot be
-# imported until we use a version of salt (or a version of the saltutils.sync_all
-# module) which supports sync'ing pillar modules on masterless hosts. See this
-# for context: https://github.com/saltstack/salt/issues/33645
-#mkdir /srv/salt-ext
-#cat <<EOF > /etc/salt/minion.d/ext_mods.conf
-#extension_modules: /srv/salt-ext
-#EOF
-# there are files in _states and _modules we want to sync and make available,
-# that is where we have our pip state/module source we've imported from upstream
-#echo "sync salt modules/states/etc from the salt file roots to the minion cache"
-#salt-call --local saltutil.sync_all
-#ls -alh /srv/salt-ext/*
